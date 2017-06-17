0f278c627668807d7529879c7287005da497e36d
Muvafika <muvafikanoor@gmail.com>
2017-06-17 19:07:41 +0000

    uptated html

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,6 +2,10 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        @todo_description = "Start Chapter Assignments for history 
+            Chapter 14-16 All Key terms&People"
+        @todo_pomodora_estimate = 4
+            
     end
     
 end
