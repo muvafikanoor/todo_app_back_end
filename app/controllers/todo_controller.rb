3335df6367757b6c82e084db1665ce58dfe8fbf3
Muvafika <muvafikanoor@gmail.com>
2017-06-18 20:30:01 +0000

    New todo add

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,7 +4,18 @@ class TodoController < ApplicationController
     def show
         todo_id = params[:id]
         @todo = Todo.find_by_id(todo_id)
-        
-        
     end
+    def new
+    end
+    def create
+        t = Todo.new
+        t.description = params["description"]
+        t.pomodoro_estimate = params["pomodoro_estimate"]
+        t.complete = false
+        t.save
+        redirect_to ("/todo/show/#{t.id}")
+    end
+        
+        
+    
 end
