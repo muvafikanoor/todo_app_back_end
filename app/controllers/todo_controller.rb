a064e1e8950dd43525143303463909ed6c77f112
Muvafika <muvafikanoor@gmail.com>
2017-06-18 18:13:05 +0000

    finished setup

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,29 +2,9 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        todo_i = params[:id]
+        todo_id = params[:id]
+        @todo = Todo.find_by_id(todo_id)
         
-        if todo_i == "1"
-        @todo_description = "Start Chapter Assignments for history 
-            Chapter 14-16 All Key terms&People"
-        @todo_pomodora_estimate = 3
-         elsif todo_i == "2"
-         
-             @todo_description = "Read any book for 10 minutes"
-             @todo_pomodora_estimate = 2
-         elsif todo_i == "3"
-         
-             @todo_description = "Study for science test"
-             @todo_pomodora_estimate = 3
-         elsif todo_i == "4"
-         
-             @todo_description = "Do KhanAcademy"
-             @todo_pomodora_estimate = 1
-        elsif todo_i == "5"
-         
-             @todo_description = "BREAK!!"
-             @todo_pomodora_estimate = 10
-         end
+        
     end
-    
 end
